6a74ed09f305827cda4b933868f4b8a315a0215a
Fathur Rohman <hi.fathur.rohman@gmail.com>
2022-06-11 08:55:32 +0700

    fix layout

--- themes/ponodo/tailwind.config.js
+++ themes/ponodo/tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./layouts/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-code': theme('colors.emerald[500]')
+          }
+        }
+      })
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
